27bf92cb6e62d9ffa0776b71d97706ae4f3b410c
Nikolai Dante <git@nathwilson.co.uk>
2024-06-03 10:57:31 +0100

    Simplify boilerplate

--- checker.py
+++ checker.py
@@ -1,37 +1,8 @@
-import logging
-import sys
-import argparse
 import os
 
 from config import BASEPATH
 
 class Checker:
-    def __init__(self):
-        parser = argparse.ArgumentParser();
-        parser.add_argument(
-            '-d', '--debug',
-            help = "Log debug information",
-            action = "store_const", dest = "loglevel", const = logging.DEBUG,
-            default = logging.WARNING
-        )
-
-        parser.add_argument(
-            '-v', '--verbose',
-            help = "Log verbose information",
-            action = "store_const", dest = "loglevel", const = logging.INFO
-        )
-
-        args = parser.parse_args();
-
-        logging.basicConfig(stream = sys.stdout, level = args.loglevel)
-        self.logger = logging.getLogger(__name__)
-
-    def writeMessage(self, message):
-        self.logger.log(logging.INFO, message)
-
-    def writeDebug(self, message):
-        self.logger.log(logging.DEBUG, message)
-
 
     def run(self):
         albums = []
@@ -44,7 +15,7 @@ class Checker:
                 if append:
                     albums.append(root)
         
-        self.writeMessage("Missing artwork:" + str(len(albums)))
+        print("Missing artwork:" + str(len(albums)))
 
         albums.sort()
         with open(BASEPATH + '/missing_art.txt', 'w') as f:
